f58344f2643d757ddad6cf25c9c000e42a800120
Alex Vear <axvr@users.noreply.github.com>
2017-04-16 22:59:40 +0100

    Initial upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# Vim - vimrc
+cp ~/Documents/Projects/dotfiles/vim/vimrc ~/.vimrc
+
+# Neovim - init.vim
+cp ~/Documents/Projects/dotfiles/neovim/init.vim ~/.config/nvim/init.vim
+
+
+# Spacemacs - spacemacs
+cp ~/Documents/Projects/dotfiles/spacemacs/spacemacs ~/.spacemacs
---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+# Vim - vimrc
+cp ~/.vimrc ~/Documents/Projects/dotfiles/vim/vimrc
+
+# Neovim - init.vim
+cp ~/.config/nvim/init.vim ~/Documents/Projects/dotfiles/neovim/init.vim
+
+
+# Spacemacs - spacemacs
+cp ~/.spacemacs ~/Documents/Projects/dotfiles/spacemacs/spacemacs
+
+
