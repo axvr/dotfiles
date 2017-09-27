4222f907617b6fb5adeef40b5ceaa70158d1883e
Alex Vear <axvr@users.noreply.github.com>
2017-09-27 13:33:57 +0100

    Added full template creation function

--- manage/dots.sh
+++ manage/dots.sh
@@ -6,12 +6,15 @@
 # Install dots on your system:
 # $ wget https://raw.githubusercontent.com/axvr/dotfiles/master/manage/dots.sh -O /usr/local/bin/dots.sh
 # $ chmod u+x /usr/local/bin/dots.sh
-# Or just run the downloaded dots.sh file
+# Or just execute the downloaded dots.sh file
 
 
 # Set location to store dotfiles
-dots_location="$HOME/Documents/Projects/dotfiles"
-mkdir -p "$dots_location"
+dotfile_location="$HOME/Documents/Projects/dotfiles"
+# File name to run
+dotdotdot_file=".dot.sh"
+# Ensure the directory exists
+mkdir -p "$dotfile_location"
 
 
 function load_dotfiles() {
@@ -24,7 +27,13 @@ function update_dotfiles() {
 }
 
 
+# Create Template function {{{
 function create_template {
+
+    arguments=("$@")
+    unset "arguments[0]"
+    # TODO validate and check there are arguments given
+
     template="#!/usr/bin/env bash
 
 function update() {
@@ -51,11 +60,63 @@ case \$1 in
 esac
 "
 
-    echo "$template" > .dot.sh
+    for selected_dir in "${arguments[@]}"
+    do
 
-}
+        if [ -f "$selected_dir/$dotdotdot_file" ]
+        then
+            printf "A '%s' file already exists in '%s'\n" \
+                "$dotdotdot_file" "$selected_dir"
+            read -r -p "Overwrite existing file (y/n)? " choice
+            case "$choice" in
+                y|Y|[yY][eE][sS] )
+                    echo "$template" > \
+                        "$dotfile_location/$selected_dir/$dotdotdot_file"
+                    printf "File '%s' in '%s' overwritten\n" \
+                        "$dotdotdot_file" "$selected_dir"
+                    ;;
+                n|N|[nN][oO] )
+                    printf "Cancelled overwrite of '%s' in '%s'\n" \
+                        "$dotdotdot_file" "$selected_dir"
+                    ;;
+                * ) 
+                    echo "Invalid option"
+                    exit
+                    ;;
+            esac
+        else
+            if [ ! -d "$dotfile_location/$selected_dir" ]
+            then
+                printf "Directory '%s' doesn't exist\n" "$selected_dir"
+                read -r -p "Create directory (y/n)? " choice
+                case "$choice" in
+                    y|Y|[yY][eE][sS] )
+                        mkdir -p "$dotfile_location/$selected_dir"
+                        printf "Directory '%s' created\n" "$selected_dir"
+                        echo "$template" > \
+                            "$dotfile_location/$selected_dir/$dotdotdot_file"
+                        printf "Template '%s' created in '%s'\n" \
+                            "$dotdotdot_file" "$selected_dir"
+                        ;;
+                    n|N|[nN][oO] )
+                        printf "Cancelled creation of directory '%s'\n" \
+                            "$selected_dir"
+                        ;;
+                    * ) 
+                        echo "Invalid option"
+                        exit
+                        ;;
+                esac
+            else
+                echo "$template" > \
+                    "$dotfile_location/$selected_dir/$dotdotdot_file"
+                printf "Template '%s' created in '%s'\n" \
+                    "$dotdotdot_file" "$selected_dir"
+            fi
+        fi
+    done
+} # }}}
 
-create_template
 
 function print_message() {
     usage_message="usage: dots <option> [dotfile name]"
@@ -67,8 +128,9 @@ Simple Dotfile Management Script:
 
 %s
 
-Options
-    -h, --help      Display this help message.
+Options:
+    -h, --help                  Display this help message.
+    -t, --template [directory]  Create a management script template.
 \n" "$usage_message"
     elif [ "$1" = "usage" ]
     then
@@ -80,12 +142,16 @@ Options
 
 
 case $1 in 
-    -h|--help)
+    -t|--template )
+        create_template "$@"
+        ;;
+    -h|--help )
         print_message "help"
         ;;
-    *)
+    * )
         print_message "usage"
         ;;
 esac
+
 
 # vim: set ts=8 sw=4 tw=80 et ft=sh fdm=marker fmr={{{,}}} :
