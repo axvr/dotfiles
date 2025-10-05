784dc73e53b08d54b9143ecded16b944d0023bda
Alex Vear <alex@vear.uk>
2025-10-05 02:57:17 +0100

    Misc. config updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,21 +6,21 @@ if status is-interactive
     set -g __fish_git_prompt_showcolorhints 1
 end
 
-set DEFAULT_VIM 'nvim'
+set --local VIM 'nvim'
 
-set --export VISUAL $DEFAULT_VIM
-set --export EDITOR $DEFAULT_VIM
-set --export MANPAGER "less --RAW-CONTROL-CHARS --use-color --color=d+y --color=u+R"
+set --export VISUAL $VIM
+set --export EDITOR $VIM
+set --export MANPAGER 'less --RAW-CONTROL-CHARS --use-color --color=d+y --color=u+R'
 
 abbr --add -- vi vim
 alias ex 'vim -E'
-alias vim $DEFAULT_VIM
+alias vim $VIM
 alias svim 'vim -S Session.vim'
-if [ $DEFAULT_VIM = 'nvim' ]; alias vimdiff 'nvim -d'; end
+if [ $VIM = 'nvim' ]; alias vimdiff 'nvim -d'; end
 
 set --export LEDGER_FILE "$HOME/Documents/Ledger/Personal.journal"
 alias ledger 'hledger'
-alias ledger-record '$EDITOR "$LEDGER_FILE"'
+alias ledger-record '$EDITOR $LEDGER_FILE'
 alias ledger-web 'echo "http://127.0.0.1:5000" | open-urls && hledger web -- --serve'
 
 set --export NOTES_DIR "$HOME/Documents/Notes"
--- vim/.vim/after/lsp/elixirls.lua
+++ vim/.vim/after/lsp/elixirls.lua
@@ -1,8 +1,17 @@
 -- TODO: switch to Elixir Expert LSP once ready.
 vim.lsp.config['elixirls'] = {
     cmd = { vim.env.HOME .. '/.local/share/mise/installs/elixir-ls/latest/language_server.sh' },
-    filetypes = { 'elixir' },
-    root_markers = { '.git', 'mix.lock', 'mix.exs' },
+    filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
+    -- Taken from: https://github.com/neovim/nvim-lspconfig/blob/e688b486fe9291f151eae7e5c0b5a5c4ef980847/lsp/elixirls.lua#L36-L45
+    root_dir = function(bufnr, on_dir)
+        local fname = vim.api.nvim_buf_get_name(bufnr)
+        -- Elixir workspaces may have multiple `mix.exs` files, for an "umbrella" layout or monorepo.
+        -- So we specify `limit=2` and treat the highest one (if any) as the root of an umbrella app.
+        local matches = vim.fs.find({ 'mix.exs' }, { upward = true, limit = 2, path = fname })
+        local child_or_root_path, maybe_umbrella_path = unpack(matches)
+        local root_dir = vim.fs.dirname(maybe_umbrella_path or child_or_root_path)
+        on_dir(root_dir)
+    end,
     settings = {
         elixirLS = {
             dialyzerEnabled = true,
